4a6dedb1ce7247db9392deadfc0fe71f2def66a8
Jukka Jylänki <jujjyl@gmail.com>
2018-11-18 12:56:24 +0200

    Put background snowflakes behind the flag

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,22 @@ void draw_frame(double t, double dt)
 {
   clear_screen(0.1f, 0.2f, 0.3f, 1.f);
 
+  // snow background
+#define NUM_FLAKES 100
+#define FLAKE_SIZE 10
+#define FLAKE_SPEED 0.05f
+#define SNOWINESS 0.998
+  static struct { float x, y; } flakes[NUM_FLAKES] = {};
+
+#define SIM do { \
+      flakes[i].y -= dt*(FLAKE_SPEED+i*0.05f/NUM_FLAKES); \
+      flakes[i].x += dt*(fmodf(i*345362.f, 0.02f) - 0.01f); \
+      float c = 0.5f + i*0.5/NUM_FLAKES; \
+      if (flakes[i].y > -FLAKE_SIZE) fill_solid_rectangle(flakes[i].x, flakes[i].y, flakes[i].x+FLAKE_SIZE, flakes[i].y+FLAKE_SIZE, c, c, c, 1.f); \
+      else if (rand01() > SNOWINESS) flakes[i].y = HEIGHT, flakes[i].x = WIDTH*rand01(); \
+    } while(0)
+  for(int i = 0; i < NUM_FLAKES/2; ++i) SIM;
+
   // flagpole x,y,width,height
 #define FPX 50.f
 #define FPW 25.f
@@ -35,22 +51,6 @@ void draw_frame(double t, double dt)
       float wy = sinf(0.3f*(x+t*0.01f))*GY*0.5f*(MIN((float)x*3.f/GX,1.f));
       fill_solid_rectangle(FX+x*BX, FY+y*BY+wy, FX+(x+1)*BX, FY+(y+1)*BY+wy, c?0.f:1.f, c?47/255.f:1.f, c?108/255.f:1.f, 1.f);
     }
-
-  // snow background
-#define NUM_FLAKES 100
-#define FLAKE_SIZE 10
-#define FLAKE_SPEED 0.05f
-#define SNOWINESS 0.998
-  static struct { float x, y; } flakes[NUM_FLAKES] = {};
-
-#define SIM do { \
-      flakes[i].y -= dt*(FLAKE_SPEED+i*0.05f/NUM_FLAKES); \
-      flakes[i].x += dt*(fmodf(i*345362.f, 0.02f) - 0.01f); \
-      float c = 0.5f + i*0.5/NUM_FLAKES; \
-      if (flakes[i].y > -FLAKE_SIZE) fill_solid_rectangle(flakes[i].x, flakes[i].y, flakes[i].x+FLAKE_SIZE, flakes[i].y+FLAKE_SIZE, c, c, c, 1.f); \
-      else if (rand01() > SNOWINESS) flakes[i].y = HEIGHT, flakes[i].x = WIDTH*rand01(); \
-    } while(0)
-  for(int i = 0; i < NUM_FLAKES/2; ++i) SIM;
 
   // ground
   fill_solid_rectangle(0.f, 0.f, WIDTH, 20.f, 0.8f, 0.8f, 0.8f, 1.f);
